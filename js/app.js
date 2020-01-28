cb7c277811a27234eac8997af40b3bfdd2596600
Nazmus Ashrafi <nazmus.s.ashrafi@gmail.com>
2020-01-28 18:33:50 +0600

    final commit v1.1

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
+const disScroll = () => {
+  const burger = document.querySelector(".nav-btn");
+  const body = document.querySelector(".body");
 
+  burger.addEventListener("click", () => {
+    body.classList.toggle("scrolldis");
+  });
+};
+
+disScroll();
